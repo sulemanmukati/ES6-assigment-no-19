f080b39fc5b4d8befe660040a7fa10ff527b2fb0
“[Suleman <[sulemanmukati@gmail.com]>
2024-09-26 10:31:46 -0700

    “[reactnativeprojectokworking]”

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+
+const App =()=>{
+  return(
+    <View>
+      <Text style={{textAlign:'center',fontSize:49,marginTop:350,color:"red"}}> Hello World </Text>
+    </View>
+  )
+}
+
+export default App;
